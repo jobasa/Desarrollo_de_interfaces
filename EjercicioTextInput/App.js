b38402ed8c64abb1e67cda5d74b3ed5613e743ad
jobasa <jobanyuls@gmail.com>
2019-10-29 17:02:32 +0100

    EjercicioTextInput terminado

--- EjercicioTextInput/App.js
+++ EjercicioTextInput/App.js
@@ -16,13 +16,8 @@ import {
   StatusBar,
   TextInput,
 } from 'react-native';
-
 import {
-  StyleSheet,
   Button,
-  View,
-  SafeAreaView,
-  Text,
   Alert,
 } from 'react-native';
 
@@ -34,68 +29,47 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
 function Separator() {
   return <View style={styles.separator} />;
 }
 
-const App= () => {
+
+const App = () => {
+  const [value, onChangeText] = React.useState('Para');
+  const [value1, onChangeText1] = React.useState('Asunto');
+  const [value2, onChangeText2] = React.useState('Mensaje');
 
   return (
     <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set
-        accessibilityLabel to help make your app usable by everyone.
-      </Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On
-        iOS, the color prop controls the color of the text. On Android, the
-        color adjusts the backgroud color of the button.
-      </Text>
-      <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        All interaction for the component are disabled.
-      </Text>
-      <Button
-        title="Press me"
-        disabled
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
+      <View>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 8 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
         />
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 8 }}
+          onChangeText1={text => onChangeText1(text)}
+          value={value1}
+        />
+
+        <TextInput
+          style={{ height: 400, width: 395, borderColor: 'gray', borderWidth: 1, margin: 8 }}
+          multiline numberOfLines={10}
+          onChangeText={text => onChangeText2(text)}
+          value={value2}
+        />
+
+
+
         <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
+          title="ENVIAR"
+          color="blue"
+          onPress={() => Alert.alert('Se ha enviado un correo a: ' + value)}
         />
       </View>
-    </View>
-  </SafeAreaView>
-
-
+      <Separator />
+    </SafeAreaView>
   );
 };
 
